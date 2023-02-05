167dd0b665330140266370ff8d05ae93154b3511
logy <logainmohamed22@gmail.com>
2023-02-05 10:15:46 +0200

    lab ui

---
+++ HallsAndLabsScheduleGenerator/Controllers/LabController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HallsAndLabsScheduleGenerator.Data;
+using ShedualeGenerator.Models;
+
+namespace HallsAndLabsScheduleGenerator.Controllers
+{
+    public class LabController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LabController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lab
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Labs.ToListAsync());
+        }
+
+        // GET: Lab/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lab = await _context.Labs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lab == null)
+            {
+                return NotFound();
+            }
+
+            return View(lab);
+        }
+
+        // GET: Lab/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lab/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Status,StudentCount,Board,Projector")] Lab lab)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lab);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lab);
+        }
+
+        // GET: Lab/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lab = await _context.Labs.FindAsync(id);
+            if (lab == null)
+            {
+                return NotFound();
+            }
+            return View(lab);
+        }
+
+        // POST: Lab/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Status,StudentCount,Board,Projector")] Lab lab)
+        {
+            if (id != lab.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lab);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LabExists(lab.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lab);
+        }
+
+        // GET: Lab/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lab = await _context.Labs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lab == null)
+            {
+                return NotFound();
+            }
+
+            return View(lab);
+        }
+
+        // POST: Lab/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var lab = await _context.Labs.FindAsync(id);
+            _context.Labs.Remove(lab);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LabExists(int id)
+        {
+            return _context.Labs.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HallsAndLabsScheduleGenerator/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HallsAndLabsScheduleGenerator.ViewModel
+{
+    public class RegisterViewModel
+    {
+          [Required]
+          [EmailAddress]
+         // [Display(Name = "Confirm Password")] 
+        public string Email { get; set; }
+          
+          [Required]
+          [DataType(DataType.Password)]
+         // [Display(Name = "Confirm Password")]
+          public string Password { get; set; }
+
+          [DataType(DataType.Password)]
+          [Display(Name = "Confirm Password")]
+          [Compare("Password", ErrorMessage = "Password and Confirm Password do not match.")]
+          public string ConfirmPassword { get; set; }
+
+    }
+}
