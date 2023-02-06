b8f68bd5976864169a11be463f6d6fcfe5b894b3
logy <logainmohamed22@gmail.com>
2023-02-06 13:43:42 +0200

    Hall Module

---
+++ HallsAndLabsScheduleGenerator/Controllers/HallController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HallsAndLabsScheduleGenerator.Data;
+using ShedualeGenerator.Models;
+
+namespace HallsAndLabsScheduleGenerator.Controllers
+{
+    public class HallController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HallController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Hall
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Halls.ToListAsync());
+        }
+
+        // GET: Hall/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hall = await _context.Halls
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hall == null)
+            {
+                return NotFound();
+            }
+
+            return View(hall);
+        }
+
+        // GET: Hall/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Hall/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,StudentCount,Status,Board,Mick,Projector")] Hall hall)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hall);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hall);
+        }
+
+        // GET: Hall/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hall = await _context.Halls.FindAsync(id);
+            if (hall == null)
+            {
+                return NotFound();
+            }
+            return View(hall);
+        }
+
+        // POST: Hall/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,StudentCount,Status,Board,Mick,Projector")] Hall hall)
+        {
+            if (id != hall.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hall);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HallExists(hall.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hall);
+        }
+
+        // GET: Hall/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hall = await _context.Halls
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hall == null)
+            {
+                return NotFound();
+            }
+
+            return View(hall);
+        }
+
+        // POST: Hall/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hall = await _context.Halls.FindAsync(id);
+            _context.Halls.Remove(hall);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HallExists(int id)
+        {
+            return _context.Halls.Any(e => e.Id == id);
+        }
+    }
+}
